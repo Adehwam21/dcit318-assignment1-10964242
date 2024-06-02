4b7b78ac748744fe9c011ec23de2f0a71bc8cd48
Adehwam21 <drewaaron96@gmail.com>
2024-06-02 19:42:25 +0000

    Update GradeCalculator files

--- GradeCalculator/obj/Debug/net8.0/GradeCalculator.AssemblyInfo.cs
+++ GradeCalculator/obj/Debug/net8.0/GradeCalculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GradeCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03cc39900a36cb5224138def823e0636939a8163")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f995710c01e3c93189e662d79a8ffff653e2e717")]
 [assembly: System.Reflection.AssemblyProductAttribute("GradeCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GradeCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
